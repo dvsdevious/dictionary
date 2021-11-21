f9758d66cb11137c1fd25e9ce3763cdd8fec18ac
dvsdevious <daria.sheremeteva@gmail.com>
2021-11-21 21:35:12 +0300

    added word definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results"
 import "./Dictionary.css"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("")
+    let [results, setResults] = useState(null)
 
     function handleResponse(response) {
-        console.log(response.data[0])
+        setResults(response.data[0])
     }
     function search(event) {
         event.preventDefault()
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
         axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event) {
@@ -18,11 +20,10 @@ export default function Dictionary() {
     }
     return (
         <div className="Dictionary">
-            <h1>Dictionary</h1>
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange}/>
             </form>
-
+            <Results results={results}/>
         </div>
     )
 }
